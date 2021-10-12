8f1e3aeb4db824683a66c1ccceb2ae343a1c4c7d
Aryas1378 <aryas1378@gmail.com>
2021-10-12 12:53:07 +0330

    two factories added

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -18,5 +22,29 @@ class CategoryTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_create_category_by_only_admin()
+    {
+
+        $role = Role::factory()->create([
+            'name' => 'admin',
+        ]);
+
+        $category = Category::factory()->create();
+
+        /** @var User $admin */
+        $admin = User::factory()->create();
+        $admin->roles()->attach($role->id);
+        $this->actingAs($admin, 'api');
+
+        $response = $this->postJson(route('admin.categories.store'),
+            [
+                'name' => $category->name,
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertSee($category->name);
+
     }
 }
